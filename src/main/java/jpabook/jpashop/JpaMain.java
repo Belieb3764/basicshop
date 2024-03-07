ca156377026bf1e25423a5330468ae14e748336f
AHRIM <belieb3764@gmail.com>
2024-03-07 15:43:39 +0900

    jpql

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,9 +4,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -20,11 +26,43 @@ public class JpaMain {
 
         try{
 
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
+            /**
+             * Criteria 사용 준비
+             */
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query =  cb.createQuery(Member.class);
 
-            em.persist(book);
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(m);
+
+            String username = "dsafas";
+            if(username !=  null) {
+                cq.where(cb.equal(m.get("username"), "kim"));
+            }
+
+            List<Member> reusultList = em.createQuery(cq)
+                    .getResultList();
+
+
+            /**
+             * JPQL
+             */
+//            List<Member> result = em.createQuery(
+//                    "select m From Member m where m.name like '%kim%'",
+//                    Member.class
+//            ).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//
+//            }
+
+//            Book book = new Book();
+//            book.setName("JPA");
+//            book.setAuthor("김영한");
+//
+//            em.persist(book);
 
 //            Order order = new Order();
 //            em.persist(order);
@@ -34,9 +72,6 @@ public class JpaMain {
 //            orderItem.setOrder(order);
 //
 //            em.persist(orderItem);
-
-
-            System.out.println();
 
             tx.commit();
         } catch (Exception e) {
